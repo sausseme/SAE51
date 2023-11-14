8f759ad5a08c5efc41ba0df3e9cef2d0d3bba36c
sausseme <emerick.saussey@univ-rouen.fr>
2023-11-14 11:24:24 +0100

    commit

---
+++ bdd.sql
@@ -0,0 +1,53 @@
+DROP DATABASE mabdd;
+CREATE DATABASE mabdd;
+USE mabdd;
+
+CREATE TABLE `Ordinateurs` (
+  `OrdinateurID` INT AUTO_INCREMENT PRIMARY KEY,
+  `UserID` INT,
+  `MarqueID` INT,
+  `DateAchat` DATE,
+  `RAM` INT,
+  `Stockage` INT,
+  `OS` VARCHAR(100)
+);
+
+CREATE TABLE `Logiciels_Installes` (
+  `Logiciels_installesID` INT,
+  `OrdinateurID` INT,
+  `LogicielID` INT
+);
+
+CREATE TABLE `Logiciels` (
+  `LogicielID` INT AUTO_INCREMENT PRIMARY KEY,
+  `Nom` VARCHAR(200)
+);
+
+CREATE TABLE `Marques` (
+  `MarqueID` INT AUTO_INCREMENT PRIMARY KEY,
+  `Nom` VARCHAR(200)
+);
+
+CREATE TABLE `Users` (
+  `UserID` INT AUTO_INCREMENT PRIMARY KEY,
+  `Nom` VARCHAR(200),
+  `Prenom` VARCHAR(200)
+);
+
+CREATE TABLE `Maintenance` (
+  `MaintenanceID` INT AUTO_INCREMENT PRIMARY KEY,
+  `Description` VARCHAR(250),
+  `OrdinateurID` INT,
+  `Action` VARCHAR(100),
+  `Date` TIMESTAMP
+);
+
+ALTER TABLE `Logiciels_Installes` ADD FOREIGN KEY (`LogicielID`) REFERENCES `Logiciels` (`LogicielID`);
+
+ALTER TABLE `Logiciels_Installes` ADD FOREIGN KEY (`OrdinateurID`) REFERENCES `Ordinateurs` (`OrdinateurID`);
+
+ALTER TABLE `Maintenance` ADD FOREIGN KEY (`OrdinateurID`) REFERENCES `Ordinateurs` (`OrdinateurID`);
+
+ALTER TABLE `Ordinateurs` ADD FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
+
+ALTER TABLE `Ordinateurs` ADD FOREIGN KEY (`MarqueID`) REFERENCES `Marques` (`MarqueID`);
--- insertion.sh
+++ insertion.sh
@@ -1,4 +1,17 @@
 mysql -u root -p'foo' -h 127.0.0.1 --port 3307 mabdd \
-	-e "LOAD DATA INFILE '/srv/csv/Ordinateurs.csv' INTO TABLE Ordinateurs FIELDS TERMINATED BY ',' IGNORE 1 ROWS (UserID,Marque,Modele,DateAchat,RAM,Stockage,OS);"
-	-e "LOAD DATA INFILE '/srv/csv/Logiciels.csv' INTO TABLE Ordinateurs FIELDS TERMINATED BY ',' IGNORE 1 ROWS (LogicielID,LogicielName,Version,LicenseKey) ;"
-MaintenanceID,OrdinateurID,Description,Action,DateAction
+	-e "LOAD DATA INFILE '/srv/csv/Logiciels.csv' INTO TABLE Logiciels FIELDS TERMINATED BY ',' IGNORE 1 ROWS (LogicielID,Nom) ;"
+
+mysql -u root -p'foo' -h 127.0.0.1 --port 3307 mabdd \	
+	-e "LOAD DATA INFILE '/srv/csv/Maitenance.csv' INTO TABLE Maintenance FIELDS TERMINATED BY ',' IGNORE 1 ROWS (MaintenanceID,OrdinateurID,Description,Action,DateAction) ;"
+
+mysql -u root -p'foo' -h 127.0.0.1 --port 3307 mabdd \	
+	-e "LOAD DATA INFILE '/srv/csv/Users.csv' INTO TABLE Users FIELDS TERMINATED BY ',' IGNORE 1 ROWS (UserID,Nom,Prenom) ;"
+
+mysql -u root -p'foo' -h 127.0.0.1 --port 3307 mabdd \
+	-e "LOAD DATA INFILE '/srv/csv/Logiciels_Installes.csv' INTO TABLE Logiciels_Installes FIELDS TERMINATED BY ',' IGNORE 1 ROWS (Logiciels_installesID,OrdinateurID,LogicielID) ;"
+
+mysql -u root -p'foo' -h 127.0.0.1 --port 3307 mabdd \
+	-e "LOAD DATA INFILE '/srv/csv/Marques.csv' INTO TABLE Logiciels_Installes FIELDS TERMINATED BY ',' IGNORE 1 ROWS (MarqueID,Nom) ;"
+
+mysql -u root -p'foo' -h 127.0.0.1 --port 3307 mabdd \
+	-e "LOAD DATA INFILE '/srv/csv/Ordinateurs.csv' INTO TABLE Ordinateurs FIELDS TERMINATED BY ',' IGNORE 1 ROWS (OrdinateurID,UserID,MarqueID,DateAchat,RAM,Stockage,OS);"
